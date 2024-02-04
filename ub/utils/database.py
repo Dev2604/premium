ecf6d51fefe2c596378f07267e5977c513360151
Dev2604 <144546659+Dev2604@users.noreply.github.com>
2024-02-05 01:49:15 +0530

    Create database.py

---
+++ ub/utils/database.py
@@ -0,0 +1,19 @@
+from pymongo import MongoClient
+from config import MONGO
+
+appdb = MongoClient(MONGO)
+db = appdb.main
+users = db.users
+
+def already_db(user_id):
+        user = users.find_one({"user_id" : str(user_id)})
+        if not user:
+            return False
+        return True
+
+def add_user(user_id):
+    in_db = already_db(user_id)
+    if in_db:
+        return
+    return users.insert_one({"user_id": str(user_id)})
+  
